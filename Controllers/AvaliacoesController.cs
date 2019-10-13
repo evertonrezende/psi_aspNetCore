91278a1ed20f84a53580a2a0dd2b11c523801c0c
Everton Rezende Costa <evertonmgti@gmail.com>
2019-10-13 13:18:32 -0300

    Add files via upload

---
+++ Controllers/AvaliacoesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PUC.LDSI.DataBase.Context;
+using PUC.LDSI.Domain.Entities;
+
+namespace PUC.LDSI.ModuloProfessor.Controllers
+{
+    public class AvaliacoesController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public AvaliacoesController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Avaliacoes
+        public async Task<IActionResult> Index()
+        {
+            var appDBContext = _context.Avaliacoes.Include(a => a.Professor);
+            return View(await appDBContext.ToListAsync());
+        }
+
+        // GET: Avaliacoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacoes
+                .Include(a => a.Professor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacoes/Create
+        public IActionResult Create()
+        {
+            ViewData["ProfessorId"] = new SelectList(_context.Professores, "Id", "Id");
+            return View();
+        }
+
+        // POST: Avaliacoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProfessorId,Disciplina,Materia,Descricao")] Avaliacao avaliacao)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(avaliacao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProfessorId"] = new SelectList(_context.Professores, "Id", "Id", avaliacao.ProfessorId);
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacoes.FindAsync(id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProfessorId"] = new SelectList(_context.Professores, "Id", "Id", avaliacao.ProfessorId);
+            return View(avaliacao);
+        }
+
+        // POST: Avaliacoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProfessorId,Disciplina,Materia,Descricao")] Avaliacao avaliacao)
+        {
+            if (id != avaliacao.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(avaliacao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AvaliacaoExists(avaliacao.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProfessorId"] = new SelectList(_context.Professores, "Id", "Id", avaliacao.ProfessorId);
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacoes
+                .Include(a => a.Professor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(avaliacao);
+        }
+
+        // POST: Avaliacoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var avaliacao = await _context.Avaliacoes.FindAsync(id);
+            _context.Avaliacoes.Remove(avaliacao);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AvaliacaoExists(int id)
+        {
+            return _context.Avaliacoes.Any(e => e.Id == id);
+        }
+    }
+}
